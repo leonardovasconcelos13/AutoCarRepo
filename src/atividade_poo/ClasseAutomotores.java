12e56859b555fdd6854b6afc8430cddf8f11e386
leonardovasconcelos13 <leonardo.sa35@etec.sp.gov.br>
2025-08-28 12:24:01 -0300

    getters e setters

--- src/atividade_poo/ClasseAutomotores.java
+++ src/atividade_poo/ClasseAutomotores.java
@@ -1,5 +1,56 @@
 package atividade_poo;
 
 public class ClasseAutomotores {
+	
+	String corAt;
+	String marcaAt;
+	String modeloAt;
+	String tipoAt;
+	
+    public ClasseAutomotores () {
+		
+	}
+	
+	public ClasseAutomotores (String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		
+	}
+	
+	public String getcorAt() {
+		return corAt;
+	}
+	
+	public void setcorAt(String corPar) {
+		this.corAt = corPar;
+	}
+	
+	
+	public String getmarcaAt() {
+		return marcaAt;
+	}
+	
+	public void setmarcaAt(String marcaPar) {
+		this.marcaAt = marcaPar;
+	}
+	
+	public String getmodeloAt() {
+		return modeloAt;
+	}
+	
+	public void setmodeloAt(String modeloPar) {
+		this.modeloAt = modeloPar;
+	}
+	
+	
+	public String gettipoAt() {
+		return tipoAt;
+	}
+	
+	public void settipoAt(String tipoPar) {
+		this.tipoAt = tipoPar;
+	}
 
 }
