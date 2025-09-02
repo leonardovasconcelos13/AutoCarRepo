429093e055a00e99053a1e7396d97574b23c0ad1
leonardovasconcelos13 <leonardo.sa35@etec.sp.gov.br>
2025-09-02 12:30:34 -0300

    Classe Carro e Caminhoes

--- src/atividade_poo/ClasseCaminhoes.java
+++ src/atividade_poo/ClasseCaminhoes.java
@@ -1,5 +1,25 @@
 package atividade_poo;
 
-public class ClasseCaminhoes {
+public class ClasseCaminhoes extends ClasseAutomotores{
+	String cargaSuportadaAt;
+	
+public ClasseCaminhoes (String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		
+		super(corPar, marcaPar, modeloPar, tipoPar);
+		
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		
+	}
+
+public String getcargaSuportadaAt() {
+	return cargaSuportadaAt;
+}
+
+public void setcargaSuportadaAt(String cargaSuportadaPar) {
+	this.cargaSuportadaAt = cargaSuportadaPar;
+}
 
 }
--- src/atividade_poo/ClasseCarros.java
+++ src/atividade_poo/ClasseCarros.java
@@ -1,5 +1,16 @@
 package atividade_poo;
 
-public class ClasseCarros {
-
+public class ClasseCarros extends ClasseAutomotores {
+ 
+	
+	public ClasseCarros (String corPar, String marcaPar, String modeloPar, String tipoPar) {
+		
+		super(corPar, marcaPar, modeloPar, tipoPar);
+		
+		this.corAt = corPar;
+		this.marcaAt = marcaPar;
+		this.modeloAt = modeloPar;
+		this.tipoAt = tipoPar;
+		
+	}
 }
